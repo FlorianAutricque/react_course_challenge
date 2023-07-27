dda4afc068c605749f2e21984dda4fed17261b6d
florian autricque <fautricque@gmail.com>
2023-07-27 13:01:27 -0400

    calculating packed items

--- 05 travel-list/src/App.js
+++ 05 travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onUpdateItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -113,10 +113,25 @@ function Item({ item, onDeleteItem, onUpdateItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list👜</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜Your have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You are ready to go ✈️🌍"
+          : `👜Your have ${numItems} items on your list, and you already packed ${numPacked} (${percentage})%`}
+      </em>
     </footer>
   );
 }
