3363784c5466a8b46167cf9edefa3474270cb086
florian autricque <fautricque@gmail.com>
2023-08-04 19:32:24 -0400

    lecutre

--- 10 React quizz/src/DateCounter.js
+++ 10 React quizz/src/DateCounter.js
@@ -3,10 +3,6 @@ import { useReducer } from "react";
 const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
-  // if (action.type === "inc") return state + 1;
-  // if (action.type === "dec") return state - 1;
-  // if (action.type === "setCount") return action.payload;
-
   switch (action.type) {
     case "dec":
       return { ...state, count: state.count - state.step };
