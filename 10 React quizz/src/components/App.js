abf862d1ff00f69656899380b4767448af45a8ba
florian autricque <fautricque@gmail.com>
2023-08-05 13:50:13 -0400

    progress bar

--- 10 React quizz/src/components/App.js
+++ 10 React quizz/src/components/App.js
@@ -6,6 +6,7 @@ import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -53,12 +54,16 @@ function reducer(state, action) {
 export default function App() {
   //question and status are destructured from state from INITIALSTATE
 
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch(`http://127.0.0.1:9000/questions`).then(res =>
@@ -81,6 +86,13 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
