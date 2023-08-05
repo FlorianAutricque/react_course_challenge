e05082b2d83a2f39dba725ec15e0e991ee7fc95e
florian autricque <fautricque@gmail.com>
2023-08-05 11:30:09 -0400

    fetching data

--- 10 React quizz/src/App.js
+++ 10 React quizz/src/App.js
@@ -1,7 +1,44 @@
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        //grab all the current state 👇
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        //return the entire state
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://127.0.0.1:9000/questions`).then(res =>
+      res
+        .json()
+        .then(data => dispatch({ type: "dataReceived", payload: data }))
+        .catch(err => dispatch({ type: "dataFailed" }))
+    );
+  }, []);
+
   return (
     <div className="app">
       <Header />
