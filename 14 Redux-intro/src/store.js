236a10a5ddc241337a9d0de6a7a2babe7bc6f29a
florian autricque <fautricque@gmail.com>
2023-08-11 17:56:35 -0400

    lecture

--- 14 Redux-intro/src/store.js
+++ 14 Redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -20,7 +22,9 @@ function reducer(state = initialState, action) {
       if (state.loan > 0) return state;
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       };
     case "account/payLoan":
       return {
@@ -34,3 +38,16 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "buy a car" },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
