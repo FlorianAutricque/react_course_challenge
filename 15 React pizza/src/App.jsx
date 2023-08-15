7fe7b57158f11a5aa17a5148683a7a99156c9cf3
florian autricque <fautricque@gmail.com>
2023-08-15 10:56:41 -0400

    setup

---
+++ 15 React pizza/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return <div>hi</div>;
+}
+
+export default App;
+
+/*
+npm create vite@latest then
+npm i then npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
+then .eslintrc.json => "extends""react-app"
+then vite config.js
+delete assets folder
+
+npm run dev
+
+*/
