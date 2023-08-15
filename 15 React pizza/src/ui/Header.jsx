31b1f574af7b41b53ef03c865c2f5c79c9a66f87
florian autricque <fautricque@gmail.com>
2023-08-15 14:09:44 -0400

    get order by id

--- 15 React pizza/src/ui/Header.jsx
+++ 15 React pizza/src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fact React Pizza Co.</Link>
+
+      <SearchOrder />
 
       <p>X</p>
     </header>
