2eb0b433216967b022dcc64b3ec8bd158dcf8759
florian autricque <fautricque@gmail.com>
2023-08-16 16:30:07 -0400

    setup redux

--- 15 React pizza/src/features/user/Username.jsx
+++ 15 React pizza/src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="test-sm hidden font-semibold md:block">Flo</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="test-sm hidden font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
