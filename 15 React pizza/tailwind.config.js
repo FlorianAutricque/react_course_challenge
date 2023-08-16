158aae4557011943dbd62fe7f32590b18f8687fd
florian autricque <fautricque@gmail.com>
2023-08-16 13:42:38 -0400

    config tailwind

--- 15 React pizza/tailwind.config.js
+++ 15 React pizza/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+//eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      height: {
+        //dvh stands for dynamic view port height
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
