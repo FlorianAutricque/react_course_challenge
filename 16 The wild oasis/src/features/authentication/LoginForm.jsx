f9cca3b4c87d5669a88e63235c0030acc80ef62b
florian autricque <fautricque@gmail.com>
2023-09-17 21:59:48 -0400

    lecture

--- 16 The wild oasis/src/features/authentication/LoginForm.jsx
+++ 16 The wild oasis/src/features/authentication/LoginForm.jsx
@@ -8,6 +8,7 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
